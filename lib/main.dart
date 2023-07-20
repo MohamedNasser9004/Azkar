71a37cc3221287b4415324b4fde054c7706665d2
Mohamed Naser <109040573+MohamedNasser9004@users.noreply.github.com>
2023-07-20 23:17:11 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:azkar/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.yellow.shade50,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.indigo,
+          textTheme: TextTheme(
+            titleSmall: TextStyle(fontFamily: "Tajawal",
+            fontSize: 22.0,
+            )
+          ),
+          centerTitle: true,
+          elevation: 0,
+        )
+      ),
+    );
+  }
+}
+
